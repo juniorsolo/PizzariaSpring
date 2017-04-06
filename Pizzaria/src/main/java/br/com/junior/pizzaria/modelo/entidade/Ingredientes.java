f6f2ac3fe11788c4ea5154c1305c830e7b77f91e
juniorsolo <junior.solo@hotmail.com>
2017-04-05 23:26:04 -0300

    Criando entidade

---
+++ Pizzaria/src/main/java/br/com/junior/pizzaria/modelo/entidade/Ingredientes.java
@@ -0,0 +1,5 @@
+package br.com.junior.pizzaria.modelo.entidade;
+
+public class Ingredientes {
+
+}
---
+++ Pizzaria/src/main/java/br/com/junior/pizzaria/modelo/enun/CategoriaIngrediente.java
@@ -0,0 +1,5 @@
+package br.com.junior.pizzaria.modelo.enun;
+
+public enum CategoriaIngrediente {
+   MOLHOS, FRIOS, SALADAS, CARNES, QUEIJOS
+}
