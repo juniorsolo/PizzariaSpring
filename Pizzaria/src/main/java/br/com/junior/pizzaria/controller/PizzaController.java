a003020e948a4ddaaacb0a18f254f27e680893e1
Manoel <manoel.silva@verity.com>
2017-04-06 17:10:54 -0300

    Usando o repositorio para aprensentar o numero de pizzas cadastradas.

--- Pizzaria/src/main/java/br/com/junior/pizzaria/controller/PizzaController.java
+++ Pizzaria/src/main/java/br/com/junior/pizzaria/controller/PizzaController.java
@@ -1,14 +1,18 @@
 package br.com.junior.pizzaria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.junior.pizzaria.modelo.repositorio.PizzaRepositorio;
+
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
-    
+     
+	 @Autowired private PizzaRepositorio pizza;
 	 public PizzaController() {
 		System.out.println("criando pizzas controller.....");
 	}
@@ -17,5 +21,11 @@ public class PizzaController {
 	@ResponseBody
 	public String ola(@PathVariable String nome){
 		return "Olá,  " + nome;
+	}
+	
+	@RequestMapping("/numero")
+	@ResponseBody
+	public String qtd(){
+		return "Existem "+ pizza.count()  +" pizzas cadastradas";
 	}
 }
