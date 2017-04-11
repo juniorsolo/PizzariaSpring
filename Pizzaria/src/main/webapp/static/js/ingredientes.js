c4b4f0e35e51ef484afefb589a026e5c083b6fb3
Manoel <manoel.silva@verity.com>
2017-04-11 16:32:39 -0300

    Criando metodo para abrir o modal para edição, limpando modal toda vez que for fechado.

--- Pizzaria/src/main/webapp/static/js/ingredientes.js
+++ Pizzaria/src/main/webapp/static/js/ingredientes.js
@@ -6,18 +6,44 @@ $(document).ready(function() {
    aplicarListeners();
 });
 
+var limparModal = function(){
+	$("#id").val("");
+	$("#nome").val("");
+	$("#categoria").val("");
+}
+
 var aplicarListeners = function() {
+	
+	$("#modal-ingrediente").on('hide.bs.modal', limparModal);
+	
+	//Buscando ingrediente
+	$(".btn-editar").on("click", function(){
+		var tr = $(this).parents('tr');
+		var id = tr.data('id');
+		var url = "ingredientes/"+id;
+		
+		$.get(url).success(function(ingrediente){
+			$("#id").val(ingrediente.id);
+			$("#nome").val(ingrediente.nome);
+			$("#categoria").val(ingrediente.categoria);
+			
+			$("#modal-ingrediente").modal('show');
+		});
+		
+	});
 	
 	//Deletando elementos da lista
 	$(".btn-deletar").on("click", function() {
 		var tr = $(this).parents('tr');
 		var id = tr.data('id');
+		var quantidade = $("#quantidade-ingredientes").text();
 		
 		$.ajax({
 			url: "ingredientes/"+id,
 			type:'DELETE',
 			success: function(result){
 				tr.remove();
+				$("#quantidade-ingredientes").text(quantidade -1);
 			}
 		});
 		
