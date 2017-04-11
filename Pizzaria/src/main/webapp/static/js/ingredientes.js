e0f5c617c9a87a94cacf2b9bba5e648e3a3d0129
Manoel <manoel.silva@verity.com>
2017-04-11 14:25:33 -0300

    Criando chamada ajax para deletar ingredientes.js

--- Pizzaria/src/main/webapp/static/js/ingredientes.js
+++ Pizzaria/src/main/webapp/static/js/ingredientes.js
@@ -1,24 +1,46 @@
 /**
- *  Criado para Ingredientes
+ * Criado para Ingredientes
  */
 
-$(document).ready(function(){
-	$("#btn-salvar").on("click", function(){
+$(document).ready(function() {
+   aplicarListeners();
+});
+
+var aplicarListeners = function() {
+	
+	//Deletando elementos da lista
+	$(".btn-deletar").on("click", function() {
+		var tr = $(this).parents('tr');
+		var id = tr.data('id');
 		
-		var formIngrediente = $("#form-ingrediente").serialize();
-		var url = 'ingredientes';
-		
-		$.post(url,formIngrediente).done(function(pagina){
-			$("#sessao-ingredientes").html(pagina);
-			
-		}).fail(function(){
-			alert('erro ao salvar');
-			
-		}).always(function(){
-			$("#modal-ingrediente").modal('hide');
+		$.ajax({
+			url: "ingredientes/"+id,
+			type:'DELETE',
+			success: function(result){
+				tr.remove();
+			}
 		});
-		
 		
 	});
 	
-});
+	//Salvando elemento e atualizando lista
+	$("#btn-salvar").on("click", function() {
+
+		var formIngrediente = $("#form-ingrediente").serialize();
+		var url = 'ingredientes';
+
+		$.post(url, formIngrediente).done(function(pagina) {
+			$("#sessao-ingredientes").html(pagina);
+			aplicarListeners();
+
+		}).fail(function() {
+			alert('erro ao salvar');
+
+		}).always(function() {
+			$("#modal-ingrediente").modal('hide');
+		});
+
+	});
+
+	
+}
