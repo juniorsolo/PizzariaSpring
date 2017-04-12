ba2c8b30bf6e02f0734c2a9f2f79709a995b6b12
Manoel <manoel.silva@verity.com>
2017-04-12 17:37:13 -0300

    Criando metodo post para salvar pizza e chamada Ajax.

--- Pizzaria/src/main/java/br/com/junior/pizzaria/controller/PizzaController.java
+++ Pizzaria/src/main/java/br/com/junior/pizzaria/controller/PizzaController.java
@@ -1,13 +1,19 @@
 package br.com.junior.pizzaria.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.junior.pizzaria.exception.PizzaInvalidException;
+import br.com.junior.pizzaria.modelo.entidade.Pizza;
 import br.com.junior.pizzaria.modelo.enun.TamanhoPizza;
 import br.com.junior.pizzaria.modelo.repositorio.PizzaRepositorio;
 
@@ -26,8 +32,22 @@ public class PizzaController {
 
 		return "pizza/listagem";
 	}
-
-
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public String salvarPizzas(Model model, @Valid @ModelAttribute Pizza pizza, 
+			BindingResult bindResult){
+		
+		if(!bindResult.hasErrors()){
+			pizzaRepo.save(pizza);
+		}else{
+			throw new PizzaInvalidException();
+		}
+		
+		model.addAttribute("tamanho", TamanhoPizza.values());
+		model.addAttribute("pizzasLista", pizzaRepo.findAll());
+		
+		return"pizza/tabela-pizzas";
+	}
 	
 	public PizzaController() {
 		System.out.println("criando pizzas controller.....");
--- Pizzaria/src/main/webapp/static/js/pizzas.js
+++ Pizzaria/src/main/webapp/static/js/pizzas.js
@@ -9,61 +9,30 @@ $(document).ready(function() {
 var limparModal = function(){
 	$("#id").val("");
 	$("#nome").val("");
-	$("#categoria").val("");
+	$("#preco").val("");
+	$("#tamanho").val("");
 }
 
 var aplicarListeners = function() {
 	
-	$("#modal-ingrediente").on('hide.bs.modal', limparModal);
+	$("#modal-pizza").on('hide.bs.modal', limparModal);
 	
-	//Buscando ingrediente
-	$(".btn-editar").on("click", function(){
-		var tr = $(this).parents('tr');
-		var id = tr.data('id');
-		var url = "ingredientes/"+id;
-		
-		$.get(url).success(function(ingrediente){
-			$("#id").val(ingrediente.id);
-			$("#nome").val(ingrediente.nome);
-			$("#categoria").val(ingrediente.categoria);
-			
-			$("#modal-ingrediente").modal('show');
-		});
-		
-	});
-	
-	//Deletando elementos da lista
-	$(".btn-deletar").on("click", function() {
-		var tr = $(this).parents('tr');
-		var id = tr.data('id');
-		var quantidade = $("#quantidade-ingredientes").text();
-		
-		$.ajax({
-			url: "ingredientes/"+id,
-			type:'DELETE',
-			success: function(result){
-				tr.remove();
-				$("#quantidade-ingredientes").text(quantidade -1);
-			}
-		});
-		
-	});
-	
+
 	//Salvando elemento e atualizando lista
 	$("#btn-salvar").on("click", function() {
 
-		var formIngrediente = $("#form-ingrediente").serialize();
-		var url = 'ingredientes';
+		var formPizza = $("#form-pizza").serialize();
+		var url = 'pizzas';
 
-		$.post(url, formIngrediente).done(function(pagina) {
-			$("#sessao-ingredientes").html(pagina);
+		$.post(url, formPizza).done(function(pagina) {
+			$("#sessao-pizzas").html(pagina);
 			aplicarListeners();
 
 		}).fail(function() {
 			alert('erro ao salvar');
 
 		}).always(function() {
-			$("#modal-ingrediente").modal('hide');
+			$("#modal-pizza").modal('hide');
 		});
 
 	});
