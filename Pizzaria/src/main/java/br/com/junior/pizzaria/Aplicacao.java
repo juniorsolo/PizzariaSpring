e374f39f3de5ea97684857ffe22ebb2073f72976
juniorsolo <junior.solo@hotmail.com>
2017-04-12 22:06:39 -0300

    Adicionando filtro para abertura de conexão nao fechar a cada requisição e sim ficar aberta para evitar o erro ao pegar a lista de ingredientes da pizza por exemplo.

--- Pizzaria/src/main/java/br/com/junior/pizzaria/Aplicacao.java
+++ Pizzaria/src/main/java/br/com/junior/pizzaria/Aplicacao.java
@@ -1,9 +1,14 @@
 package br.com.junior.pizzaria;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,14 +17,27 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class Aplicacao implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		   AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-		   applicationContext.setConfigLocation("br.com.junior.pizzaria.config");
-		   
-		   Dynamic appServlet = servletContext.addServlet("appServlet", new DispatcherServlet(applicationContext));
-		   appServlet.setLoadOnStartup(1);
-		   appServlet.addMapping("/app/*");
-		   
-		   servletContext.addListener(new ContextLoaderListener(applicationContext));
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+		applicationContext.setConfigLocation("br.com.junior.pizzaria.config");
+
+		Dynamic appServlet = servletContext.addServlet("appServlet", new DispatcherServlet(applicationContext));
+		appServlet.setLoadOnStartup(1);
+		appServlet.addMapping("/app/*");
+
+		servletContext.addListener(new ContextLoaderListener(applicationContext));
+		
+		FilterRegistration.Dynamic filter = servletContext.addFilter("openEntityManagerFilter", buildOpenEntityManagerFilter());
+		filter.addMappingForUrlPatterns(getDispatcherTypes(), false, "/*");
+		
 	}
-  
+
+	private OpenEntityManagerInViewFilter buildOpenEntityManagerFilter() {
+		OpenEntityManagerInViewFilter openEntityManagerInViewFilter = new OpenEntityManagerInViewFilter();
+		openEntityManagerInViewFilter.setEntityManagerFactoryBeanName("entityManagerFactory");
+		return openEntityManagerInViewFilter;
+	}
+
+	private EnumSet<DispatcherType> getDispatcherTypes() {
+		return EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ASYNC);
+	}
 }
