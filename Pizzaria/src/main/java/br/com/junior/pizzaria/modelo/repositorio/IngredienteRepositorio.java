973ddf499f09b740b015df0c0393c8791f3be3b7
juniorsolo <junior.solo@hotmail.com>
2017-04-24 15:00:43 -0300

    Criando novos metodos para o repositorio.

--- Pizzaria/src/main/java/br/com/junior/pizzaria/modelo/repositorio/IngredienteRepositorio.java
+++ Pizzaria/src/main/java/br/com/junior/pizzaria/modelo/repositorio/IngredienteRepositorio.java
@@ -1,11 +1,18 @@
 package br.com.junior.pizzaria.modelo.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.junior.pizzaria.modelo.entidade.Ingrediente;
+import br.com.junior.pizzaria.modelo.entidade.Pizzaria;
 
 @Repository
 public interface IngredienteRepositorio extends CrudRepository<Ingrediente, Long>{
+	
+	public List<Ingrediente> findAllByDono(Pizzaria dono);
+	
+	public Ingrediente findByIdAndDono(Long id, Pizzaria dono);
 	
 }
